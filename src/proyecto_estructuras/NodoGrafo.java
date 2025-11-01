8ba2fdb520239dce7c511bbb4900ee5e17e2e324
Anthony Goncalves <anthony.goncalves@correo.unimet.edu.ve>
2025-11-01 11:41:54 -0400

    Segundo commit: clase ListaAdyacencia.

---
+++ src/proyecto_estructuras/NodoGrafo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_estructuras;
+
+/**
+ *
+ * @author Antho
+ */
+public class NodoGrafo {
+    protected String data;
+    protected ListaAdyacencia listaAdyacente;
+    protected NodoGrafo siguiente;
+    boolean Visitado;
+    
+
+    public NodoGrafo(String x) {
+        this.data = x;
+        listaAdyacente = new ListaAdyacencia();
+        this.siguiente = null;
+        Visitado = false;
+    }
+    
+        
+    
+    
+    
+    
+}
