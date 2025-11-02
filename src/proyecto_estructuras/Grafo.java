0153cf225f47cb97fa64c94ac74845eebdd33a58
Anthony Goncalves <anthony.goncalves@correo.unimet.edu.ve>
2025-11-02 12:43:41 -0400

    Tercer commit: Implementacion del metodo eliminar nodo, buscar nodo, eliminar arista en la clase grafo.

--- src/proyecto_estructuras/Grafo.java
+++ src/proyecto_estructuras/Grafo.java
@@ -8,11 +8,11 @@ package proyecto_estructuras;
  *
  * @author Antho
  */
-public class Grafo {
+public class Grafo{
     private NodoGrafo primero;
     private NodoGrafo ultimo;
 
-    public Grafo() {
+    public Grafo(){
         this.primero = null;
         this.ultimo = null;
     }
@@ -65,9 +65,9 @@ public class Grafo {
     
     public String Recorrer(){
         NodoGrafo pAux = this.primero;
-        String Cadena ="";
-        while(pAux != null){
-            Cadena = Cadena + pAux.data +" -> Es amigo de "+ pAux.listaAdyacente.Recorrer() + "\n";
+        String Cadena = "";
+        while (pAux != null){
+            Cadena = Cadena + pAux.data + " -> ha agregado a " + pAux.listaAdyacente.Recorrer() + "\n";
             pAux = pAux.siguiente;
         }
         return Cadena;
@@ -75,8 +75,71 @@ public class Grafo {
     
     
     
+    private NodoGrafo BuscarNodo(String dato){
+        if (this.GrafoEsvacio()){
+            return null;
+        }
+        NodoGrafo pBuscador = this.primero;
+        while (pBuscador != null){
+            if (pBuscador.data.equals(dato)){
+                return pBuscador;
+            }
+            pBuscador = pBuscador.siguiente;
+        }
+        return null;
+    }
+    
+
+    public void EliminarArista(String origen, String destino){
+        NodoGrafo pAux = this.BuscarNodo(origen);
+
+        if (pAux != null){
+            pAux.listaAdyacente.EliminarAdyacencia(destino);
+        } else {
+            System.out.println("El perfil >" + origen + "< no existe. Por tanto no se puede eliminar su amistad con el perfil >"+ destino + "<.");
+        }
+    }
     
     
+    public void EliminarNodo(String perfil){
+        if (this.GrafoEsvacio()){
+            return;
+        }
+
+        NodoGrafo pBuscador = this.primero;
+        while (pBuscador != null){
+            pBuscador.listaAdyacente.EliminarAdyacencia(perfil);
+            pBuscador = pBuscador.siguiente;
+        }
+
+        if (this.primero.data.equals(perfil)){
+            this.primero = this.primero.siguiente;
+
+            if (this.primero == null){
+                this.ultimo = null;
+            }
+            return;
+        }
+
+        NodoGrafo pPrev = this.primero;
+        pBuscador = this.primero.siguiente;
+
+        while (pBuscador != null && !pBuscador.data.equals(perfil)){
+            pPrev = pBuscador;
+            pBuscador = pBuscador.siguiente;
+        }
+        if (pBuscador != null && pBuscador.data.equals(perfil)){
+            pPrev.siguiente = pBuscador.siguiente;
+            if (pBuscador == this.ultimo){
+                this.ultimo = pPrev;
+            }
+
+        } else {
+            System.out.println("El perfil >" + perfil + "< que se deseaba eliminar no existe.");
+        }
+    }
+
+         
     
     
     
